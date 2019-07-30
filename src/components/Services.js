d145104df1aa5141179cb1cbdf3c5193ce8a88ac
kanmiragi <fredrickbdn@gmail.com>
2019-07-30 12:34:32 +0100

    react beach application

---
+++ src/components/Services.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import {FaCocktail, FaHiking, FaShuttleVan, FaBeer} from 'react-icons/fa';
+import Title from './Title'
+
+export default class Services extends Component {
+    state={
+        services: [
+            {
+                icon: <FaCocktail />,
+                title: "Free Cocktails",
+                info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, inventore?'
+            },
+            {
+                icon: <FaHiking />,
+                title: "Endless Hiking",
+                info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, inventore?'
+            },
+            {
+                icon: <FaShuttleVan />,
+                title: "Free shuttle",
+                info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, inventore?'
+            },
+            {
+                icon: <FaBeer />,
+                title: "Strongest Beer",
+                info: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum, inventore?'
+            }
+        ]
+    }
+    render() {
+        return (
+            <section className="services">
+                <Title title="services" />
+                <div className="services-center">
+                    {this.state.services.map((item, index) => {
+                        return <article key={index} className="service">
+                            <span>{item.icon}</span>
+                            <h6>{item.title}</h6>
+                            <p>{item.info}</p>
+                            </article>
+                    })}
+                </div>
+            </section>
+        )
+    }
+}
